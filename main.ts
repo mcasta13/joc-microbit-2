cf5e152cbca6a80da8d5d5515df2e7167ddfbb44
mcasta13 <116083045+mcasta13@users.noreply.github.com>
2022-11-07 21:14:09 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,18 @@ input.onButtonPressed(Button.A, function () {
     if (SPRITE.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
     } else {
-        game.gameOver()
+        if (true) {
+            game.removeLife(1)
+        } else {
+            game.gameOver()
+        }
     }
 })
 let SPRITE: game.LedSprite = null
 game.startCountdown(10000)
 SPRITE = game.createSprite(2, 2)
 game.setScore(0)
+game.setLife(3)
 basic.forever(function () {
     SPRITE.move(1)
     basic.pause(200)
